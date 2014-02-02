64d932209c040b9a7ab4fa52143bf0f227f521b8
ibeaumont <ibeaumont@zubirimanteo.com>
2014-02-02 22:52:50 +0100

    cors

--- app.js
+++ app.js
@@ -9,12 +9,32 @@ var express  = require("express"),
     mongoose = require('mongoose'),
     path = require( 'path' ),
     application_root = __dirname;
-    
+
+//prueba cors    
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+//fin prueba cors
    
 app.configure(function () {
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
+  //prueba cors
+  app.use(allowCrossDomain);
+  app.use(express.static(path.join(application_root, "public")));
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+  //fin prueba cors
 });
 
 //app.get('/', function(req, res) {
